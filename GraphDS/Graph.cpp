a39bf9b809b798f2b23f4c8ebd2ad2fcb279008d
Garima Chhikara <garima@Garimas-MacBook-Pro.local>
2024-04-24 09:27:35 +0530

    Graph

--- GraphDS/Graph.cpp
+++ GraphDS/Graph.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<queue>
+#include<stack>
+#include<vector>
 
 using namespace std ;
 
@@ -81,11 +83,287 @@ class Graph
 
         return false ;
     }
+
+    void BFT()
+    {
+        queue<int> q ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        for(int src=0 ; src < V ; src++)
+        {
+            if(visited[src])
+                continue ;
+
+            q.push(src) ;
+            while(!q.empty())
+            {
+                // remove
+                int rv = q.front() ;
+                q.pop() ;
+
+                if(visited[rv])
+                    continue ;
+
+                // visited
+                visited[rv] = true ;
+
+                // print
+                cout << rv << " " ;
+
+                // nbrs
+                for(int j = 0 ; j < V ; j++)
+                {
+                    if(matrix[rv][j] != 0 && !visited[j])
+                    {
+                        q.push(j) ;
+                    }
+                }
+
+            }
+        }
+
+        cout << endl ;
+    }
+
+    bool DFS(int src, int dst)
+    {
+        stack<int> s ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        s.push(src) ;
+        while(!s.empty())
+        {
+            // remove
+            int rv = s.top() ;
+            s.pop() ;
+
+            if(visited[rv])
+                continue ;
+
+            // visited
+            visited[rv] = true ;
+
+            // check dst ?
+            if(rv == dst)
+                return true ;
+
+            // nbrs
+            for(int j = 0 ; j < V ; j++)
+            {
+                if(matrix[rv][j] != 0 && !visited[j])
+                {
+                    s.push(j) ;
+                }
+            }
+
+        }
+
+        return false ;
+    }
+
+    void DFT()
+    {
+        stack<int> s ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        for(int src=0 ; src < V ; src++)
+        {
+            if(visited[src])
+                continue ;
+
+            s.push(src) ;
+            while(!s.empty())
+            {
+                // remove
+                int rv = s.top() ;
+                s.pop() ;
+
+                if(visited[rv])
+                    continue ;
+
+                // visited
+                visited[rv] = true ;
+
+                // print
+                cout << rv << " " ;
+
+                // nbrs
+                for(int j = 0 ; j < V ; j++)
+                {
+                    if(matrix[rv][j] != 0 && !visited[j])
+                    {
+                        s.push(j) ;
+                    }
+                }
+
+            }
+        }
+
+        cout << endl ;
+    }
+
+
+    bool isConnected()
+    {
+        // count is calculating the no. of components
+        int count = 0 ;
+
+        queue<int> q ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        for(int src=0 ; src < V ; src++)
+        {
+            if(visited[src])
+                continue ;
+
+            q.push(src) ;
+            count++ ;
+            while(!q.empty())
+            {
+                // remove
+                int rv = q.front() ;
+                q.pop() ;
+
+                if(visited[rv])
+                    continue ;
+
+                // visited
+                visited[rv] = true ;
+
+                // print
+                // cout << rv << " " ;
+
+                // nbrs
+                for(int j = 0 ; j < V ; j++)
+                {
+                    if(matrix[rv][j] != 0 && !visited[j])
+                    {
+                        q.push(j) ;
+                    }
+                }
+
+            }
+
+        }
+
+        // cout << endl ;
+
+        return count == 1 ;
+    }
+
+    bool isCyclic()
+    {
+        queue<int> q ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        for(int src=0 ; src < V ; src++)
+        {
+            if(visited[src])
+                continue ;
+
+            q.push(src) ;
+            while(!q.empty())
+            {
+                // remove
+                int rv = q.front() ;
+                q.pop() ;
+
+                if(visited[rv])
+                    return true ;
+
+                // visited
+                visited[rv] = true ;
+
+                // print
+                // cout << rv << " " ;
+
+                // nbrs
+                for(int j = 0 ; j < V ; j++)
+                {
+                    if(matrix[rv][j] != 0 && !visited[j])
+                    {
+                        q.push(j) ;
+                    }
+                }
+
+            }
+        }
+
+        // cout << endl ;
+        return false ;
+    }
+
+    bool isTree()
+    {
+        return isConnected() && !isCyclic() ;
+    }
+
+    vector< vector<int> > getConnectedComponents()
+    {
+        vector<vector<int> > bv ;
+        queue<int> q ;
+        // bool visited[V] ;
+        bool *visited = new bool[V] ;
+        for(int i = 0 ; i < V ; i++)
+            visited[i] = false ;
+
+        for(int src=0 ; src < V ; src++)
+        {
+            if(visited[src])
+                continue ;
+
+            vector<int> sv ;
+            q.push(src) ;
+            while(!q.empty())
+            {
+                // remove
+                int rv = q.front() ;
+                q.pop() ;
+
+                if(visited[rv])
+                    continue ;
+
+                // visited
+                visited[rv] = true ;
+
+                // print
+                sv.push_back(rv) ;
+
+                // nbrs
+                for(int j = 0 ; j < V ; j++)
+                {
+                    if(matrix[rv][j] != 0 && !visited[j])
+                    {
+                        q.push(j) ;
+                    }
+                }
+            }
+            bv.push_back(sv) ;
+
+        }
+
+        return bv ;
+    }
 } ;
 
 int main()
 {
-    Graph g(7) ;
+    Graph g(10) ;
 
     g.addEdge(0,1,10) ;
     g.addEdge(1,2,20) ;
@@ -96,8 +374,31 @@ int main()
     g.addEdge(5,6,70) ;
     g.addEdge(4,6,80) ;
 
+    g.addEdge(7,8,80) ;
+    g.addEdge(8,9,80) ;
+    g.addEdge(7,9,80) ;
+
     g.display() ;
 
-    cout << g.BFS(0,6) << endl ;
+    // cout << g.DFS(0,8) << endl ;
+    // g.BFT() ;
+    // g.DFT() ;
 
+    cout << g.isConnected() << endl ;
+    cout << g.isCyclic() << endl ;
+    cout << g.isTree() << endl ;
+
+    vector< vector<int> > res = g.getConnectedComponents() ;
+
+    for(int i = 0 ; i < res.size() ; i++)
+    {
+        vector<int> sv = res[i] ;
+        for(int j = 0 ; j < sv.size() ; j++)
+        {
+            cout << sv[j] << " " ;
+        }
+        cout << endl ;
+    }
+
+    return 0 ;
 }
